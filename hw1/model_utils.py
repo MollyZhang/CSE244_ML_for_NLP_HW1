0f6e37d038ee11ed9548b2a8c1a5a35cad27465e
Molly <mollyzhang@ucsc.edu>
2020-02-05 14:53:59 -0800

    bert finish

--- hw1/model_utils.py
+++ hw1/model_utils.py
@@ -6,9 +6,23 @@ import torch
 import numpy as np
 import re
 import os
-from bert_embedding import BertEmbedding
+from transformers import BertTokenizer, BertForSequenceClassification
 
 
+class BertMovie(nn.Module):
+    def __init__(self, output_dim=46):
+        super().__init__()
+        self.tokenizer = BertTokenizer.from_pretrained('bert-base-uncased')
+        self.bert = BertForSequenceClassification.from_pretrained(
+            'bert-base-uncased', num_labels=output_dim)      
+
+    def forward(self, x):
+        _, extra = x
+        tokenized_text = self.tokenizer.tokenize(extra["raw_text"][0])
+        print(tokenized_text)
+        print("haha")
+
+        return None
 
 
 class BaseModel(nn.Module):
@@ -29,59 +43,6 @@ class BaseModel(nn.Module):
         preds = self.final_layer(emb)
         return preds
 
-
-class BaseModelWithLabel(nn.Module):
-    """add label features"""
-    def __init__(self, pretrained_emb=False, device="cuda",
-                 emb_dim=300, hidden_dim=300, output_dim=46, vocab_size=2044):
-        super().__init__()
-        if pretrained_emb:
-            emb_matrix = torch.load("./data/emb_matrix_ft.pt")
-            self.embedding = nn.Embedding.from_pretrained(emb_matrix, 
-                freeze=False)
-        else: 
-            self.embedding = nn.Embedding(vocab_size, emb_dim)
-        self.label_vocab = np.load("./data/label_vocab.npy")       
-        self.labels = np.load("./data/labels.npy") 
-        self.final_layer1 = nn.Linear(emb_dim, output_dim)
-        #self.linear_layer = nn.Linear(emb_dim, hidden_dim)
-        self.final_layer2 = nn.Linear(len(self.labels), output_dim)
-        self.combination_layer = nn.Linear(2*output_dim, output_dim)
-        self.device = device
-        self.output_dim = output_dim
- 
-    def forward(self, x):
-        seq, raw_text = x
-        # emb dimension: batch_size * emb_dim
-        emb = self.embedding(seq).sum(dim=0) # sum of word embedding
-        # label_feature: batch_size * label_vocab
-        label_features = self.create_label_feature(raw_text)
-        preds1 = self.final_layer1(emb)
-        preds2 = self.final_layer2(label_features)
-        scaling_bias = torch.rand(self.output_dim, requires_grad=True).to(self.device)
-        combined_preds = self.combination_layer(torch.cat((preds1, preds2), dim=1))
-        return combined_preds
-
-    def create_label_feature(self, text):
-        """ a label get additional boosts if a word in text overlaps with it
-            boost num is the number of words which overlaps with this label
-        """
-        label_feature = torch.zeros(len(text), self.output_dim)
-        for i, each_line in enumerate(text):
-            for word in re.split("'| ", each_line):
-                if word in self.label_vocab:
-                    for j, each_label in enumerate(self.labels):
-                        if word in each_label:
-                            label_idx = np.where(self.labels==each_label)[0][0]
-                            label_feature[i, label_idx] += 1
-        if self.device == "cuda":
-            return label_feature.cuda()
-        elif self.device == "cpu":
-            return label_feature
-
-    def create_cosim_feature(self, text):
-        """ cosine similarity between text and label words"""
-        pass
 
 
 class GRU(nn.Module):
@@ -179,32 +140,4 @@ class MultiLayerMLP(nn.Module):
         preds = self.final_layer(hidden)
         return preds
 
-
-class BertMLP(nn.Module):
-    def __init__(self, path="data",
-                 emb_dim=768,
-                 hidden_dim=400,  
-                 output_dim=46,
-                 num_middle_layer=0,
-                 ):
-        super().__init__()
-        self.first_layer = nn.Linear(emb_dim, hidden_dim)
-        self.middle_layers = []
-        for _ in range(num_middle_layer):
-            self.middle_layers.append(nn.Linear(hidden_dim, hidden_dim))
-        self.middle_layers = nn.ModuleList(self.middle_layers)
-        self.final_layer = nn.Linear(hidden_dim, output_dim)
-        self.batchnorm = nn.BatchNorm1d(hidden_dim)
-        self.bert_embedding = BertEmbedding()
-        
-    def forward(self, seq):
-        (emb, ngram), extra = seq
-        raw_text = extra["raw_text"]
-        emb = self.embedding(emb).sum(dim=0) # sum of word embedding
-        hidden = F.relu(self.batchnorm1(self.first_layer(emb)))
-        for i, middle_layer in enumerate(self.middle_layers):
-            hidden = F.relu(self.batchnorm2(middle_layer(hidden)))
-                
-        preds = self.final_layer(hidden)
-        return preds
 
